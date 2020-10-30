d7513247e81af161bfe3fd5b80e1718c4e9160ca
Haneen Amro <heno.amro@gmail.com>
2020-10-29 19:28:10 -0700

    added new url

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,55 +1,37 @@
+
+
+
+
 const express = require('express');
+const  {createProxyMiddleware}  = require('http-proxy-middleware');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const path = require('path');
-const axios = require('axios');
-
+const cors = require('cors')
 const app = express();
-const port = 5000;
+const port = 3000;
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// Middleware
+app.use(cors())
+app.use(morgan('dev'));
+
+
+
+// Serves static HTML file
 app.use(express.static(path.join(__dirname + '/../public')));
 
 
+app.use( '/api/reviews/:id',createProxyMiddleware({ target: 'http://54.241.88.76:5555/', changeOrigin: true }));
+app.use( '/photos/:id',createProxyMiddleware({ target: 'http://54.176.6.154:1337/3', changeOrigin: true }));
+//app.use( '/favorites',createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
 
-app.get('/api/reviews/:id', (req, res) => {
+app.use( '/getRestaurants',createProxyMiddleware({ target: 'http://18.144.30.151:8000/', changeOrigin: true }));
+app.use( '/api/more-places',createProxyMiddleware({ target: 'http://54.176.6.154:1337/3', changeOrigin: true }));
+// Verifies and sets port on where server is listens at
 
-  axios.get(`http://13.52.246.213/api/reviews/${req.params.id}`, { params: { text: req.query.text } })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(401).send(err);
-    });
-
-
-});
-
-app.get('/photos/:id', (req, res) => {
-
-  axios.get(`http://18.216.243.227/photos/${req.params.id}`)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(401).send(err);
-    });
-
-
-});
-
-app.get('/getRestaurants', (req, res) => {
-
-  axios.get(`http://34.212.131.243/getRestaurants`)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(401).send(err);
-    });
-
-});
-
-
-
-
-app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Listening on proxy port ${port}!`));
